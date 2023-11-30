83df5fa30ed3896bf1862b27f4944798c1a9aafb
Jesse-Unruh-Garcia <98713768+Jesse-Unruh-Garcia@users.noreply.github.com>
2023-11-30 18:28:44 -0500

    Search is Swag Nasty now

--- search_media/views.py
+++ search_media/views.py
@@ -13,50 +13,57 @@ def search_media(request):
 
             search_results = Media.objects.raw(
             f"""
-            SELECT DISTINCT media.spotify_id, type, media.name, artist_name, overall_rating
-            FROM media, album NATURAL JOIN artist, song
+            SELECT DISTINCT media.spotify_id, type, media.name, overall_rating, artist_name, album_type, album_art
+            FROM media, album NATURAL JOIN artist, song 
             WHERE name LIKE '%%{search_keyword}%%'
             AND (spotify_id = album.album_id 
-                OR spotify_id = song_id)
+                OR (spotify_id = song_id AND album.album_id=song.album_id))
+            ORDER BY {filter} {order}
             """
             )
             return render(request, 'search_media/media.html',
                 {'search_form': search_form, 'search_results': search_results,})
     else:
         search_form = SearchMediaForm()
+    """
+        OR spotify_id = podcast.podcast_id 
+                OR (spotify_id = episode_id AND episode.podcast_id = episode.episode_id)
+
+                for when we want podcasts
+    """
 
     return render(request, 'search_media/media.html',
                 {'search_form': search_form,})
 
-def view_reviews(request):
-    if request.method == "POST":
-        search_form = SearchMediaForm(request.POST)
+# def view_reviews(request):
+#     if request.method == "POST":
+#         search_form = SearchMediaForm(request.POST)
 
-        if search_form.is_valid():
-            search_keyword = search_form.cleaned_data["search_keyword"]
+#         if search_form.is_valid():
+#             search_keyword = search_form.cleaned_data["search_keyword"]
 
-            search_results = Media.objects.raw(
-                f"""
-                    SELECT DISTINCT first, last , review.spotify_id, name, title, star_rating, text
-                    FROM review NATURAL JOIN sleeves_user, media
-                    WHERE name LIKE '%%{search_keyword}%%' and review.spotify_id = media.spotify_id
-                """
-            )
-            return render(request, 'search_media/review.html',
-                          {'review_form' : search_form, 'review_results' : search_results})
-    else:
-        search_form = SearchMediaForm()
-        search_results = Media.objects.raw(
-                f"""
-                    SELECT DISTINCT first, last , review.spotify_id, name, title, star_rating, text
-                    FROM review NATURAL JOIN sleeves_user, media    
-                    WHERE review.spotify_id = media.spotify_id
-                    ORDER BY star_rating DESC
-                """
-            )
+#             search_results = Media.objects.raw(
+#                 f"""
+#                     SELECT DISTINCT first, last , review.spotify_id, name, title, star_rating, text
+#                     FROM review NATURAL JOIN sleeves_user, media
+#                     WHERE name LIKE '%%{search_keyword}%%' and review.spotify_id = media.spotify_id
+#                 """
+#             )
+#             return render(request, 'search_media/review.html',
+#                           {'review_form' : search_form, 'review_results' : search_results})
+#     else:
+#         search_form = SearchMediaForm()
+#         search_results = Media.objects.raw(
+#                 f"""
+#                     SELECT DISTINCT first, last , review.spotify_id, name, title, star_rating, text
+#                     FROM review NATURAL JOIN sleeves_user, media    
+#                     WHERE review.spotify_id = media.spotify_id
+#                     ORDER BY star_rating DESC
+#                 """
+#             )
 
-    return render(request, 'search_media/review.html',
-                          {'review_form' : search_form, 'review_results' : search_results})
+#     return render(request, 'search_media/review.html',
+#                           {'review_form' : search_form, 'review_results' : search_results})
 
 
 def user_search(request):
