f06fd3ea95efafec9b809154325ab61d48c31705
Jesse-Unruh-Garcia <98713768+Jesse-Unruh-Garcia@users.noreply.github.com>
2023-12-01 01:56:04 -0500

    Revert "Merge branch 'searchpage'"
    This reverts commit a84300959b2cbeb937246348b36395867448f82a, reversing
    changes made to c8f38baeeac1c805b54152372e7e5d3a17d5a9b0.

--- pages/views.py
+++ pages/views.py
@@ -1,27 +1,3 @@
 from django.shortcuts import render
-from sleeves.models import SleevesUser, Review
 
 # Create your views here.
-def user_page(request, user_id):
-    user_object  = SleevesUser.objects.raw(
-    f"""
-    SELECT *
-    FROM sleeves_user
-    WHERE user_id = '{user_id}'
-    """
-    )[0]
-
-    user_reviews = Review.objects.raw(
-        f"""
-        SELECT title, text, star_rating, sleeves_user.user_id 
-        FROM sleeves_user, review 
-        WHERE sleeves_user.user_id = '{user_id}'
-        AND review.user_id = '{user_id}';
-        """
-    )
-
-
-    return render(request, 'pages/user.html',
-                          {'user' : user_object, 'reviews' : user_reviews})
-
-    
