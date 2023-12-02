6598d95a0ef9df129735b05e962cd71f6c1c2b17
tjburk <ty_burkhardt@verizon.net>
2023-12-01 22:26:11 -0500

    Delete review functionality done

--- user_page/urls.py
+++ user_page/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import user_page
+from .views import user_page, delete_review
 
 
 urlpatterns = [
     path('<user_id>', user_page, name="user_page"),
+    path('delete/<user_id>/<spotify_id>/<title>', delete_review, name="delete_review"),
 ]
--- user_page/views.py
+++ user_page/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from sleeves.models import SleevesUser, Review
+from django.db import connection
 
 def user_page(request, user_id):
     user_object  = SleevesUser.objects.raw(
@@ -12,15 +13,24 @@ def user_page(request, user_id):
 
     user_reviews = Review.objects.raw(
         f"""
-        SELECT title, text, star_rating, sleeves_user.id, review.user_id
+        SELECT title, text, star_rating, sleeves_user.id, review.user_id, spotify_id
         FROM sleeves_user, review 
-        WHERE sleeves_user.id = '{user_id}'
-        AND review.user_id = '{user_id}';
+        WHERE sleeves_user.id = {user_id}
+        AND review.user_id = {user_id};
         """
     )
-
 
     return render(request, 'user_page/user_page.html',
                           {'user' : user_object, 'reviews' : user_reviews})
 
+
+def delete_review(request, user_id, spotify_id, title):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f"""
+            DELETE FROM review
+            WHERE user_id = {user_id} AND spotify_id = "{spotify_id}" AND title = "{title}";
+            """
+        )
     
+    return user_page(request, user_id)
