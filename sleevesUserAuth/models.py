d3bac205aec7b75a60a6267be5b83b32aba053e1
tjburk <ty_burkhardt@verizon.net>
2023-12-02 14:14:33 -0500

    Mild refactoring

--- sleevesUserAuth/models.py
+++ sleevesUserAuth/models.py
@@ -1,9 +1,3 @@
 from django.db import models
-from django import forms 
 
 # Create your models here.
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
--- sleevesUserAuth/views.py
+++ sleevesUserAuth/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render, redirect
-from sleeves.models import SleevesUser, Review
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-
-# Create your views here.
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import LoginForm
-from django.contrib import messages
-
+from .forms import LoginForm
 
 
 def user_login(request):
